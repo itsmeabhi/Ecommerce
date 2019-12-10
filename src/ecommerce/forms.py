8facfbceb4843ae705430ca7ab6b34a8205fc841
abhijeet <itsmeabhi1796@gmail.com>
2019-12-10 21:32:27 +0530

    Added files

---
+++ src/ecommerce/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': "form-control",
+                   'id': "form_full_name",
+                   'placeholder': "Your Full Name"
+                   }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'your Email ID'
+                   }
+        )
+    )
+    contact = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'class': 'form-control',
+                   'placeholder': 'your Information'
+                   }
+        )
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if "gmail.com" in email:
+            return email
+        raise forms.ValidationError("Email has to be gmail.com")
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
---
+++ src/ecommerce/views.py
@@ -0,0 +1,55 @@
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm
+
+
+def home_page(request):
+    context = {
+        'content': 'Home Page'
+    }
+    return render(request, "homepage.html", context)
+
+
+def contact_page(request):
+    import ipdb
+    ipdb.set_trace()
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        'content': 'Contact Page',
+        'form': contact_form,
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    return render(request, "contact/view.html", context)
+
+
+def about_page(request):
+    context = {
+        'content': 'About Page'
+    }
+    return render(request, "homepage.html", context)
+
+
+def login_page(request):
+    login_form = LoginForm(request.POST or None)
+    context = {
+        'form': login_form
+    }
+    print request.user.is_authenticated()
+    if login_form.is_valid():
+        print login_form.cleaned_data
+        username = login_form.cleaned_data.get('username')
+        password = login_form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/login")
+        else:
+            print "Error"
+
+    return render(request, "auth/login.html", context)
+
+
+
+
